f52de0ca062a96d2de1834e4ab045d9f0a58d045
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-23 23:29:17 +0700

    Fix layout of dashboard login page

--- app/controllers/dashboard/sessions_controller.rb
+++ app/controllers/dashboard/sessions_controller.rb
@@ -2,6 +2,8 @@ module Dashboard
   class SessionsController < ::DashboardController
     skip_before_action :authenticate_user!, only: [:new, :create]
 
+    layout "application"
+
     def new
       redirect_to dashboard_path if user_signed_in?
     end
