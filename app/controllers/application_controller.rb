2b1f95c09e543bc1c4cbb16900aa54896594b90c
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-23 23:27:38 +0700

    Add user login feature and logout back-end codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def authenticate_user!
+    redirect_to dashboard_login_path unless user_signed_in?
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  helper_method :current_user
 end
