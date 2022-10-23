a5ccb21c3738109ca1170ff69336f436f2be57d3
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-23 23:19:10 +0700

    Add dashboard index

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// AlpineJS
+import Alpine from "alpinejs"
+window.Alpine = Alpine
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  window.Alpine.start();
+});
